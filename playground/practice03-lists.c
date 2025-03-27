056f030cd388e3f13ac5ed4f0d8ed0c9433c3087
gris <185072671+d-branco@users.noreply.github.com>
2025-03-27 13:44:41 +0000

    linked list refresher
    the file 03 on the playground was used to refresh how it all works

---
+++ playground/practice03-lists.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   practice03-lists.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 09:23:00 by abessa-m          #+#    #+#             */
+/*   Updated: 2025/03/27 13:43:36 by abessa-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+typedef enum e_token_type
+{
+	TOKEN_ARG,
+	TOKEN_QUOTE,
+	TOKEN_PIPE
+}	t_e_token_type;
+
+typedef struct s_token
+{
+	t_e_token_type	type;
+	char			*token_string;
+	int				length;
+}	t_s_token;
+
+t_s_token	*make_a_token(
+					t_e_token_type type, const char *token_string, int length)
+{
+	t_s_token	*data;
+
+	data = malloc(sizeof(t_s_token));
+	if (!data)
+		return (NULL);
+	data->type = type;
+	data->token_string = strdup(token_string);
+	data->length = length;
+	return (data);
+}
+
+void	print_token_list_info(void *content)
+{
+	t_s_token	*data;
+	const char	*type_strings[] = {"argument(s)", "quote", "length"};
+
+	data = (t_s_token *)content;
+	printf(
+		"Token struct	Token type: %s, Token string: %s, Token length: %d\n",
+		type_strings[data->type], data->token_string, data->length);
+}
+
+void	delete_token(void *content)
+{
+	t_s_token	*data;
+
+	data = (t_s_token *)content;
+	free(data->token_string);
+	free(data);
+}
+
+void	*iterate_function(void *content)
+{
+	t_s_token	*old_data;
+	t_s_token	*new_data;
+	char		modified_token_string[256];
+
+	old_data = (t_s_token *)content;
+	new_data = malloc(sizeof(t_s_token));
+	new_data->type = old_data->type;
+	new_data->length = old_data->length * 2;
+	snprintf(modified_token_string, sizeof(modified_token_string),
+		"Modified %s", old_data->token_string);
+	new_data->token_string = strdup(modified_token_string);
+	return (new_data);
+}
+
+int	main(void)
+{
+	t_list	*list;
+	t_list	*iterated_list;
+
+	list = NULL;
+	ft_lstadd_back(&list, ft_lstnew(make_a_token(TOKEN_ARG, "echo", 4)));
+	ft_lstadd_back(&list, ft_lstnew(make_a_token(TOKEN_QUOTE, "", 0)));
+	ft_lstadd_back(&list, ft_lstnew(make_a_token(TOKEN_QUOTE, "\"", 1)));
+	printf("Original List:\n");
+	ft_lstiter(list, print_token_list_info);
+	printf("\nList Size: %d\n", ft_lstsize(list));
+	iterated_list = ft_lstmap(list, iterate_function, delete_token);
+	printf("\nList after iterating over every member:\n");
+	ft_lstiter(iterated_list, print_token_list_info);
+	ft_lstclear(&list, delete_token);
+	ft_lstclear(&iterated_list, delete_token);
+	return (0);
+}
