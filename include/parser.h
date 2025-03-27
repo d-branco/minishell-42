a7bf74ff728cbcd6924d61b0c2b46616aae05737
gris <185072671+d-branco@users.noreply.github.com>
2025-03-27 13:56:42 +0000

    norminette

--- include/parser.h
+++ include/parser.h
@@ -6,8 +6,22 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 13:46:07 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/03/26 14:27:48 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/03/27 13:51:44 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+typedef enum e_token_type
+{
+	TOKEN_ARG,
+	TOKEN_QUOTE,
+	TOKEN_PIPE
+}	t_e_token_type;
+
+typedef struct s_token
+{
+	t_e_token_type	type;
+	char			*token_string;
+	int				length;
+}	t_s_token;
