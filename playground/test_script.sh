3640396171b743f690dac62877c2ed7c7f8d4aa5
gris <185072671+d-branco@users.noreply.github.com>
2025-04-02 16:44:13 +0100

    small organization

--- playground/test_script.sh
+++ playground/test_script.sh
@@ -6,18 +6,18 @@ make minishell
 
 echo -e "=========================================="\
 			"                             MINISHELL"
-./exp_minishell.exp "$@" > out_minishell.txt
+./playground/exp_minishell.exp "$@" > out_minishell.txt
 sed -i 's/\x1B\[[0-9;?]*[a-zA-Z]//g' out_minishell.txt
 cat out_minishell.txt
 sleep 0.2
 echo -e "=========================================="\
 			"                                  BASH"
-./exp_bash.exp "$@" > out_bash.txt
+./playground/exp_bash.exp "$@" > out_bash.txt
 sed -i 's/\x1B\[[0-9;?]*[a-zA-Z]//g' out_bash.txt
 cat out_bash.txt
 echo -e "=========================================="\
 			"                                  diff"
-diff out_minishell.txt out_bash.txt
+diff -B -c out_minishell.txt out_bash.txt
 DIFF_EXIT_CODE=$?
 
 rm -f out_minishell.txt out_bash.txt
