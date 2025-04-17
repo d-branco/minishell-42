956b0e2fab7aad7b3b07bb3d80c83c21a7aac8d0
gris <185072671+d-branco@users.noreply.github.com>
2025-04-17 18:02:10 +0100

    single pipe execution

--- src/execution/exe.c
+++ src/execution/exe.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:07:05 by abessa-m          #+#    #+#             */
-/*   Updated: 2025/04/17 16:23:30 by abessa-m         ###   ########.fr       */
+/*   Updated: 2025/04/17 18:00:13 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	execute_command(t_ast_node *node);
 int	execute_and(t_ast_node *node);
 int	execute_or(t_ast_node *node);
+int	execute_pipe(t_ast_node *node);
 
 int	execute_ast(t_ast_node *node)
 {
@@ -23,8 +24,8 @@ int	execute_ast(t_ast_node *node)
 	handle_exit_code(0);
 	if (node->type == AST_COMMAND)
 		handle_exit_code(execute_command(node));
-	//else if (node->type == AST_PIPE)
-	//	handle_exit_code(execute_pipe(node));
+	else if (node->type == AST_PIPE)
+		handle_exit_code(execute_pipe(node));
 	//else if (node->type == AST_REDIRECT)
 	//	handle_exit_code(execute_redirect(node));
 	else if (node->type == AST_AND)
@@ -52,14 +53,17 @@ int	execute_command(t_ast_node *node)
 	if (pid == 0)
 	{
 		if (DEBUG)
-			printf("--DEBUG--Hurray! A new child is born!\n");
+		{
+			printf("--DEBUG-- Hurray! A new child is born!\n");
+			print_ast(node, 0);
+		}
 		execvp(cmd->command, cmd->args);
 		fprintf(stderr, "minishell: %s: command not found\n", cmd->command);
 		handle_exit_code(127);
 		if (DEBUG)
-			printf("--DEBUG-- Did they had to die today?\n");
+			printf("--DEBUG-- Did a child had to be killed today?\n");
 		free_ast_node(node);
-		exit (127);
+		exit (handle_exit_code(-1));
 	}
 	else if (pid < 0)
 	{
@@ -91,3 +95,80 @@ int	execute_or(t_ast_node *node)
 		return (execute_ast(node->right));
 	return (left_status);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+static void	run_left_child(t_ast_node *node, int *pipefd)
+{
+	close(pipefd[0]);
+	dup2(pipefd[1], STDOUT_FILENO);
+	close(pipefd[1]);
+	if (DEBUG)
+	{
+
+		printf("--DEBUG-- Did the left child had to be killed today?\n");
+		print_ast(node->left, 0);
+	}
+	handle_exit_code(execute_ast(node->left));
+	free_ast_node(node);
+	exit (handle_exit_code(-1));
+}
+
+static void	run_right_child(t_ast_node *node, int *pipefd)
+{
+	close(pipefd[1]);
+	dup2(pipefd[0], STDIN_FILENO);
+	close(pipefd[0]);
+	if (DEBUG)
+	{
+
+		printf("--DEBUG-- Did the right child had to be killed today?\n");
+		print_ast(node->right, 0);
+	}
+	handle_exit_code(execute_ast(node->right));
+	free_ast_node(node);
+	exit (handle_exit_code(-1));
+}
+
+static int	wait_for_child(pid_t pid)
+{
+	int	status;
+
+	waitpid(pid, &status, 0);
+	if (DEBUG)
+		printf("--DEBUG-- __________________________________________\n");
+	if (WIFEXITED(status))
+		return (handle_exit_code(WEXITSTATUS(status)));
+	return (handle_exit_code(1));
+}
+
+int	execute_pipe(t_ast_node *node)
+{
+	int		pipefd[2];
+	pid_t	left_pid;
+	pid_t	right_pid;
+	int		right_status;
+
+	if (pipe(pipefd) == -1)
+	{
+		perror("minishell: pipe");
+		return (1);
+	}
+	left_pid = fork();
+	if (left_pid == -1)
+		return (perror("minishell: fork"), 1);
+	if (left_pid == 0)
+		run_left_child(node, pipefd);
+	right_pid = fork();
+	if (right_pid == -1)
+		return (perror("minishell: fork"), 1);
+	if (right_pid == 0)
+		run_right_child(node, pipefd);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	wait_for_child(left_pid);
+	right_status = wait_for_child(right_pid);
+	return (handle_exit_code(right_status));
+}
+
+////////////////////////////////////////////////////////////////////////////////
